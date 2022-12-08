d2873e4209374a534e9663e58e22277f3634204e
Michaela Wells <mgwells012@gmail.com>
2022-12-08 11:19:26 -0500

    about me section has the outline added to it, an additional tab was added for github link directly, a photo was added into the about me page, the resume tab should link directly to the resume in git hub

--- src/components/b-navbar.js
+++ src/components/b-navbar.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import { Navbar } from 'react-bootstrap';
+
 
 function LinkTab(props) {
   return (
@@ -34,10 +34,11 @@ export default function NavTabs() {
 >
         
         <LinkTab label="Home" href="/" underline="hover" />
+        <LinkTab label="Github" href="https://github.com/MWells012" underline="hover" />
         <LinkTab label="About the Dev" href="/aboutMe" underline="hover" />
         <LinkTab label="Portfolio" href="/portfolio" underline="hover" />
         <LinkTab label="Contact" href="/contact" underline="hover" />
-        <LinkTab label="Resume" href="/resume" underline="hover" />
+        <LinkTab label="Resume" href="https://github.com/MWells012/20-React-Portfolio/blob/main/src/components/body/downloads/Michaela%20Wells%20-%20Resume.pdf" underline="hover" />
       </Tabs>
     </Box>
   );
--- src/components/body/c-about-me.js
+++ src/components/body/c-about-me.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css'
+import Photo from './assets/photo-of-Michaela.jpg'
 
 const About = () => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <React.Fragment> 
+       
+      
+          <div className="container bg-light" style={{marginTop: '100px'}}>
+         
+            <h3 className="text-warning" style={{marginTop:'70px', textAlign:'center', fontSize: '50px'}}> Hi! I'm Michaela.</h3>
+              <div className="row ">
+                <div className="col-lg-5 col-md-6 col-sm-12 order-sm-first order-last">
+                  
+                  <div className="text-white-50">
+                  <img style={{marginTop: '70px', marginBottom: '40px',  marginLeft:"40px", width: '300px', height: '400px',}} src={Photo} alt="photo of michaela, blonde hair black shirt in a field of tulips with a large old fashioned windmill in the background"></img>
+                    
+                
+                  </div>
+                </div>  
+                <div className="col-lg-7 col-md-6 col-sm-12 text-dark" style={{marginTop: '70px', fontSize: '20px'}}>
+                    <p> . </p>
+                    <p>. </p>
+                    <p> .</p>
+                </div>
+            </div>
+          </div>
+          
+        </React.Fragment>
+      );
+  };
 
 export default About;
