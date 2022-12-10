60abd811b064d65d204d1489ae63743694e3aab5
Michaela Wells <mgwells012@gmail.com>
2022-12-09 19:40:31 -0500

    photos and links have been added for the most part in the portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../App.css'
+import '../App.css';
+import NoteTaker from './assets/note-taker.png';
+import ProfileGenerator from './assets/profile-generator.png'
+import ReadMe from './assets/readme-generator.png'
+import QuestionGame from './assets/question-game.png'
+import TimeSchedule from './assets/time-schedule.png'
 
 import Carousel from 'react-bootstrap/Carousel';
-import Card from 'react-bootstrap/Card'; 
-import { Container } from 'react-bootstrap';
+
 
 
 
@@ -24,83 +28,88 @@ const Portfolio = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://mdbootstrap.com/img/Photos/Slides/img%20(19).jpg"
+          src= { TimeSchedule }
           alt="First slide"
         />
         <Carousel.Caption>
           <div style={{ background:'#D4D4D4', opacity:'85%'}}>
-            <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href=''>View on Github</a></h4>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum. </p>
+            <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href='https://github.com/MWells012/5-TimeToPlan'>View on Github</a></h4>
+            <p> Time Scheduler </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
 
+
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg"
+          src= {ReadMe}
           alt="Second slide"
         />
         <Carousel.Caption>
           <div style={{ background:'#D4D4D4', opacity:'85%'}}>
-          <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href=''>View on Github</a></h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href='https://github.com/MWells012/9-Generate-Your-ReadMe'>View on Github</a></h4>
+            <p> Generate a Professional ReadMe </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://mdbootstrap.com/img/Photos/Slides/img%20(40).jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <div style={{ background:'#D4D4D4', opacity:'85%'}}>
-          <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href=''>View on Github</a></h4>
-            <p> Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </div>
-        </Carousel.Caption>
-      </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://mdbootstrap.com/img/new/slides/041.jpg"
+          src= { }
           alt="Third slide"
         />
         <Carousel.Caption>
           <div style={{ background:'#D4D4D4', opacity:'85%'}}>
-          <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href=''>View on Github</a></h4>
-            <p> Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href='https://github.com/MWells012/12-Employee-Tracker'>View on Github</a></h4>
+            <p> Employee Tracker </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://mdbootstrap.com/img/new/slides/042.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <div style={{ background:'#D4D4D4', opacity:'85%'}}>
-          <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href=''>View on Github</a></h4>
-            <p> Praesent commodo cursus magna, vel scelerisque nisl consectetur. </p>
-          </div>
-        </Carousel.Caption>
-      </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://mdbootstrap.com/img/new/slides/043.jpg"
+          src= {QuestionGame}
           alt="Third slide"
         />
         <Carousel.Caption>
           <div style={{ background:'#D4D4D4', opacity:'85%'}}>
-          <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href=''>View on Github</a></h4>
-            <p> Praesent commodo cursus magna, vel scelerisque nisl consectetur. </p>
+            <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href='https://github.com/MWells012/4-TheQuestionGame'>View on Github</a></h4>
+            <p> Question / Quiz </p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src= {ProfileGenerator}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <div style={{ background:'#D4D4D4', opacity:'85%'}}>
+            <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href='https://github.com/MWells012/10-Team-Profile-Generator'>View on Github</a></h4>
+            <p> Team Profile Generator </p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src= {NoteTaker}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <div style={{ background:'#D4D4D4', opacity:'85%'}}>
+            <h4 style={{paddingTop:'2pt'}}><a class='btn border-dark' href='https://github.com/MWells012/11-Note-Taker'>View on Github</a></h4>
+            <p> Note Taker </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
